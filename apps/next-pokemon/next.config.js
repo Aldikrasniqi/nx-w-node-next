709d55c27a66905159258dddfef016ce0f2e5046
Aldikrasniqi <aldikrasniqi5@gmail.com>
2024-07-11 13:28:57 +0200

    Adding ssr support

--- apps/next-pokemon/next.config.js
+++ apps/next-pokemon/next.config.js
@@ -12,11 +12,14 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  pageExtensions: ['tsx', 'ts'],
+  reactStrictMode: true,
 };
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
